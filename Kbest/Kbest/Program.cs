370c37e6d08c2f609b0dfa0308352fcb41b16862
Stephan Kolesnikov <ksa8552855@gmail.com>
2025-09-28 19:02:47 +0300

    Two solutions added

---
+++ Kbest/Kbest/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbest
+{
+    public class Jewel
+    {
+        public int Index { get; private set; }
+        public int V { get; private set; }
+        public int W { get; private set; }
+        public Jewel(int index, int v, int w)
+        {
+            Index = index;
+            V = v;
+            W = w;
+        }
+    }
+    internal class Program
+    {
+        static List<int> Check(Jewel[] jws, int k, double x)
+        {
+            double[] diff = new double[jws.Length];
+            for (int i = 0; i < jws.Length; ++i)
+            {
+                diff[i] = jws[i].V - x * jws[i].W;
+            }
+
+            int[] indeces = new int[jws.Length];
+            for (int i = 0; i < jws.Length; ++i) { indeces[i] = i; }
+            Array.Sort(indeces, (a, b) => diff[b].CompareTo(diff[a]));
+
+            List<int> selected = new List<int>();
+            for (int i = 0; i < k; ++i)
+            {
+                selected.Add(jws[indeces[i]].Index);
+            }
+
+            return selected; 
+        }
+        static void Main(string[] args)
+        {
+            string[] ts = Console.ReadLine().Split();
+            int n = int.Parse(ts[0]);
+            int k = int.Parse(ts[1]);
+
+            Jewel[] jewels = new Jewel[n];
+            for (int i = 0; i < n; i++)
+            {
+                ts = Console.ReadLine().Split();
+                jewels[i] = new Jewel(i + 1, 
+                    int.Parse(ts[0]),
+                    int.Parse(ts[1]));
+            }
+
+            double left = 0, right = 1e6,
+                eps = 1e-7;
+
+            List<int> ans = null;
+
+            while (right - left > eps)
+            {
+                double mid = (right + left) / 2.0;
+                List<int> cand = Check(jewels, k, mid);
+
+                double sumDiff = 0;
+                foreach (var idx in cand)
+                {
+                    sumDiff += jewels[idx - 1].V - mid * jewels[idx - 1].W;
+                }
+
+                if (sumDiff > 0)
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+                ans = cand;
+            }
+
+            foreach (int idx in ans)
+            {
+                Console.WriteLine(idx);
+            }
+        }
+    }
+}
---
+++ TernarySearch/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernarySearchTask
+{
+    internal class Program
+    {
+        static double T(double x, double a, int Vp, int Vf)
+        {
+            return Math.Sqrt(x * x + (1 - a) * (1 - a)) / Vp +
+                Math.Sqrt((1 - x) * (1 - x) + a * a) / Vf;
+        }
+
+        static double TernarySearch(double left, double right, double eps,
+            double a, int Vp, int Vf)
+        {
+            while (right - left > eps)
+            {
+                double mid1 = left + (right - left) / 3;
+                double mid2 = right - (right - left) / 3;
+
+                if (T(mid1, a, Vp, Vf) < T(mid2, a, Vp, Vf))
+                {
+                    right = mid2;
+                }
+                else
+                {
+                    left = mid1;
+                }
+            }
+
+            return (left + right) / 2;
+        }
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int velocityP = int.Parse(tokens[0]);
+            int velocityF = int.Parse(tokens[1]);
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{TernarySearch(0, 1, 1e-7, a, velocityP, velocityF):F6}");
+        }
+    }
+}
