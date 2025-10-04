14e3e5673b33993fe86cf733aef9ea16f17849d8
Stephan Kolesnikov <ksa8552855@gmail.com>
2025-10-04 11:34:30 +0300

    Full first contest

---
+++ KSumm/KSumm/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSumm
+{
+    internal class Program
+    {
+        static long UpperBound(int[] b, long value)
+        {
+            int left = 0, right = b.Length;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (b[mid] <= value)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        static void BinarySummary(int n, 
+            int[] firstArr, int[] secondArr, long selected)
+        {
+            long left = firstArr.Min() + secondArr.Min();
+            long right = firstArr.Max() + secondArr.Max();
+            long ans = 0;
+
+            while (left <= right)
+            {
+                long mid = left + (right - left) / 2;
+                if (CountPairs(firstArr, secondArr, mid) >= selected)
+                {
+                    ans = mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+        
+        static long CountPairs(int[] a, int[] b, long x)
+        {
+            long result = 0;
+            foreach (var valA in a)
+            {
+                long remaining = x - valA;
+                result += UpperBound(b, remaining);
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int n = int.Parse(tokens[0]);
+            long k = long.Parse(tokens[1]);
+
+            int[] ai = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bi = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(bi);
+            
+            BinarySummary(n, ai, bi, k);
+        }
+    }
+}
---
+++ TableOfMultiplies/TableOfMultiplies/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableOfMultiplies
+{
+    internal class Program
+    {
+        static long countLess(int n, long k)
+        {
+            long count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                long val = Math.Min(n, k / i);
+                count += val;
+            //    Console.WriteLine($"i={i}, k/i={k / i}, min={val}");
+            }
+            return count;
+        }
+
+        static long TableBinarySearch(int n, long k)
+        {
+            long left = 0, right = (long)n * n;
+            while (left < right)
+            {
+                long middle = (left + right) / 2;
+                if (countLess(n , middle) < k)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return left;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int n = int.Parse(tokens[0]);
+            long k = long.Parse(tokens[1]);
+
+            Console.WriteLine(TableBinarySearch(n, k));
+        }
+    }
+}
